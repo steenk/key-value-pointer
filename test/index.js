33afb3c1c19a7aba9a1882a81f6702d2522f8e68
Steen Klingberg <steen@sk-ab.se>
2015-02-04 12:28:32 +0530

    tests

--- test/index.js
+++ test/index.js
@@ -49,6 +49,15 @@ describe('Testing KVP', function () {
 			});
 			assert(res === 'doggy');
 		});
+		it('should return changed value', function () {
+			var res = k.query(function (node) {
+				if (node.key === 'aaa') {
+					node.value.y = 'YYY';
+					return true;
+				}
+			});
+			assert(o['aa']['aaa']['y'] === 'YYY');
+		});
 		it('should return undefined withour error', function () {
 			var res = k.query(function () {});
 			assert(res === undefined);
@@ -56,13 +65,25 @@ describe('Testing KVP', function () {
 	});
 
 	describe('select tests', function () {
-		it('should return node value "YY"', function () {
-			var res = k.select('/aa/aaa/y');
-			assert(res === 'YY');
+		it('should return node value "ex"', function () {
+			var res = k.select('/aa/aaa/x');
+			assert(res === 'ex');
 		});
 		it('should return node value 5', function () {
 			var res = k.select('/d/4');
 			assert(res === 5);
+		});
+	});
+
+	describe('replace tests', function () {
+		it('should return false', function () {
+			k.replace('/e', false);
+			assert(!o['e']);
+		});
+		it('should return true', function () {
+			k.replace('/e', !k.select('/e'));
+			var res = k.select('/e');
+			assert(res);
 		});
 	});
 })
